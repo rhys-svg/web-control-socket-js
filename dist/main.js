0991b804bb5c75a4258130d7d00904ad3f8ca2d9
Rhys Mason-Gorrie <rhys@backtothe.uk>
2025-12-13 15:46:28 +0000

    Initial Project creation

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Basic Build Command
+console.group('Application Initalisation');
+console.log('Hello World.');
+//# sourceMappingURL=main.js.map
